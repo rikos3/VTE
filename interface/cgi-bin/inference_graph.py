c559ff34d0ffee83a46ac7eae636afc506aa44a9
RIko Suzuki <g1520544@is.ocha.ac.jp>
2019-10-25 21:25:42 +0900

    add files for interface

---
+++ interface/cgi-bin/inference_graph.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from os.path import expanduser
+HOME = expanduser("~")
+
+C2L = HOME + "/ccg2lambda"
+MMI = HOME + "/multimodal_inference"
+
+import cgi
+
+import sys
+sys.path.append(C2L + "/scripts")
+sys.path.append(MMI + "/scripts")
+
+import subprocess
+import time
+import math
+
+from lxml import etree
+from nltk.sem.logic import *
+from visualization_tools import convert_root_to_mathml
+from word2num import word_to_num as w2n
+
+import theoremProve_prover9_graph as tpp9
+import model_check as mc
+import make_compound as mcomp
+
+def prove(fol):
+    start = time.time()
+    lst = tpp9.theoremProve_prover9(fol)
+    end = time.time()
+    return(lst, end-start)
+
+def model_check(fol):
+    start = time.time()
+    lst = mc.model_check_graph(fol)
+    end = time.time()
+    return(lst, end-start)
+
+print('Content-type: text/html\n')
+
+html = """
+<!DOCTYPE html>
+<meta charset="utf-8" />
+<html>
+<head>
+<title>output</title>
+<style>
+h1 {
+font-size: 1.5em;
+}
+h2 {
+font-size: 1em;
+}
+</style>
+</head>
+<body>
+<input type="button" onclick="location.href='../index.html'" value="戻る"><br>
+<center>%s</center>
+</body>
+"""
+
+# get fol
+form = cgi.FieldStorage()
+input_text = form.getfirst('input','')
+
+# select parser
+parser = form.getfirst('parser','')
+
+if input_text == 'text':
+    text = form.getvalue('text','')
+    text = w2n(text)
+    with open(MMI + '/input.txt', 'w') as f:
+        f.write(text)
+    start = time.time()
+    res = subprocess.run([MMI + "/scripts/parse_multi.sh",parser], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    fol = res.stdout.decode('utf-8')
+    end = time.time()
+    time1 = end-start
+elif input_text == 'fol':
+    text = form.getvalue('text','')
+    fol = text
+    time1 = 0
+
+fol = mcomp.make_compound(fol)
+
+body_sem = '<h1> Input: ' + text + '<br> FOL: ' + fol + '</h1><br>'
+print(html % (body_sem))
+
+html_images = """
+<center>%s</center>
+</html>
+"""
+
+# select model_check / prover
+selector = form.getfirst('select','')
+if selector == 'model_check':
+    lst, time2 = model_check(fol)
+elif selector == 'prover':
+    lst, time2 = prove(fol)
+    #lst = [x.decode('utf-8').replace("\n","") for x in lst]
+
+num = len(lst)
+body = '<h2>' + selector + '<br>' \
+    + 'Number of images: ' + str(num) \
+    + '&nbsp; Parsing: ' + str(round(time1)) + ' sec' \
+    + '&nbsp; Proving: ' + str(round(time2)) + ' sec</h2><br>'
+for image in lst:
+    img = '<img src="../visual_genome/image/' + image + '.jpg" title="' + image + '" height="150">&nbsp;'
+    body += img
+print(html_images % (body))
+
+with open(MMI + "/input.txt." + parser + ".sem.xml") as f:
+    root = etree.parse(f)
+    html_str = convert_root_to_mathml(root)
+    print(html_str)
